1c0ec62abd424e4c76495bda3f338e73ebc78568
saktiworkstation <saktiworkstation@gmail.com>
2024-02-22 14:44:48 +0700

    Create an Api Book Controller and run the index method

---
+++ app/Http/Controllers/Api/BukuController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Buku;
+use Illuminate\Http\Request;
+
+class BukuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Buku::orderBy('judul', 'asc')->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'Data ditemukan.',
+            'data' => $data
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
