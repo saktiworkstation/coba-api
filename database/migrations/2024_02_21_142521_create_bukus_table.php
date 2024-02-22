2c5fd3547f00f96027e53c5fa7f643c82ee9b033
saktiworkstation <saktiworkstation@gmail.com>
2024-02-22 13:18:23 +0700

    Complete the migration schema

--- database/migrations/2024_02_21_142521_create_bukus_table.php
+++ database/migrations/2024_02_21_142521_create_bukus_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('bukus', function (Blueprint $table) {
+        Schema::create('buku', function (Blueprint $table) {
             $table->id();
+            $table->string('judul');
+            $table->string('pengarang');
+            $table->string('tanggal_publikasi');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('bukus');
+        Schema::dropIfExists('buku');
     }
 };
